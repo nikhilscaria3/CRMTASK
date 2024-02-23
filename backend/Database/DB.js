54d5f9167638f4c9dc3fb965da91dd52165dc1ca
NIkhil Scaria <125331845+nikhilscaria3@users.noreply.github.com>
2024-02-23 10:56:06 +0530

    Update DB.js

--- backend/Database/DB.js
+++ backend/Database/DB.js
@@ -4,18 +4,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Check if the current environment is development
-    if (process.env.NODE_ENV === 'development') {
-      // Connect to the local MongoDB database
-      const url = process.env.DB_LOCAL_URI;
-      await mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('MongoDB is connected to local');
-      console.log('MongoDB URL:', url);
-    } else {
-      // Connect to the MongoDB Atlas database
+ // Connect to the MongoDB Atlas database
       const url = process.env.DB_LOCAL_ATLASURI;
       await mongoose.connect(url, {
         useNewUrlParser: true,
@@ -23,7 +12,7 @@ const connectDB = async () => {
       });
       console.log('MongoDB is connected to Atlas');
       console.log('MongoDB URL:', url);
-    }
+    
   } catch (error) {
     console.error('Error connecting to the database:', error.message);
   }
